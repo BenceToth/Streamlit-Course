4710cfc254ea2466257373516a3d7e2984da8a1e
Bence L Toth <bence.l.toth@gmail.com>
2025-01-13 16:44:38 +0100

    generate synthetic bar progress

--- synthetic-data-generator_app.py
+++ synthetic-data-generator_app.py
@@ -11,3 +11,13 @@ status_text = st.sidebar.empty()
 # Init Chart
 last_rows = np.random.randn(1,1)
 chart = st.line_chart(last_rows)
+
+# Generate synthetic data
+for i in range(1, 101):
+    new_rows = last_rows[-1,:] + np.random.randn(5,1)
+    status_text.text("%i%% Complete"%i)
+    progress_bar.progress(i)
+    
+    time.sleep(0.1)
+    
+progress_bar.empty()
