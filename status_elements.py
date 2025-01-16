4667dbdeff8618b5c8b41a7899b5f1f6f19bbab6
BenceToth <bence.l.toth@gmail.com>
2025-01-16 13:16:07 +0000

    add spinner

--- status_elements.py
+++ status_elements.py
@@ -5,9 +5,14 @@ import time
 st.header('st.progress')
 st.caption('Display a progress bar')
 
-my_bar = st.progress(0)
-
-# update value in loop
-for pct_complete in range(1, 101):
-    time.sleep(0.5)
-    my_bar.progress(pct_complete)
+def show_progbar():
+    # update value in loop
+    for pct_complete in range(1, 121, 20):
+        time.sleep(0.5)
+        pct_complete = min(pct_complete, 100)
+        my_bar.progress(pct_complete)
+    
+# Spinner
+with st.spinner("Something is processing"):
+    my_bar = st.progress(0)
+    show_progbar()
