720932eeed610d6e1be505e0b84290848e26ab76
BenceToth <bence.l.toth@gmail.com>
2025-01-16 13:21:39 +0000

    display different types of messages

--- status_elements.py
+++ status_elements.py
@@ -16,3 +16,20 @@ def show_progbar():
 with st.spinner("Something is processing"):
     my_bar = st.progress(0)
     show_progbar()
+   
+# Info
+st.subheader('st.info')
+st.info('This is an Information message')
+
+st.subheader('st.success')
+st.success('This is a Success message')
+
+st.subheader('st.warning')
+st.warning('This is a Warning message')
+
+st.subheader('st.error')
+st.error('This is an Error message')
+
+time.sleep(2)
+# Fly balloons :)
+st.balloons()
