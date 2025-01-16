0e7b3e0f769621b150dd1902bb66c27f2de79625
BenceToth <bence.l.toth@gmail.com>
2025-01-16 14:54:57 +0000

    display 5 random rows from a df is a button is clicked

--- 2_api/input_widgets.py
+++ 2_api/input_widgets.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+
+# load data
+tips_df = pd.read_csv('tips.csv')
+
+def display_random_sample(df):
+    sample_data = df.sample(5)
+    return sample_data
+
 
 # Button widget
-button = st.button('Press Me')
-st.write('button = ', button)
+st.subheader('Display 5 random rows from tips.csv')
+st.caption('Click on the button below to display the rows randomly')
+button = st.button('Display random 5 rows')
+
+if button:
+    sample_tips = display_random_sample(tips_df)
+    st.dataframe(sample_tips)
