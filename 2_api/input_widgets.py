d0dd4af467c191cdeae7d2c10593fa33085df7e7
BenceToth <bence.l.toth@gmail.com>
2025-01-16 15:10:10 +0000

    submit multiple checkbox container to JSON

--- 2_api/input_widgets.py
+++ 2_api/input_widgets.py
@@ -19,3 +19,36 @@ button = st.button('Display random 5 rows')
 if button:
     sample_tips = display_random_sample(tips_df)
     st.dataframe(sample_tips)
+ 
+st.markdown('---')   
+
+# Checkbox widget
+st.subheader('st.checkbox')
+is_agreed = st.checkbox('I agree to terms and conditions')  # returns bool
+st.write('checkbox status: {}'.format(is_agreed))
+
+# Multiple checkboxes
+with st.container():
+    st.info('What technologies do you use?')
+    
+    python = st.checkbox('Python')
+    datascience = st.checkbox('Data Science')
+    ai_ml = st.checkbox('AI/ML')
+    andorid = st.checkbox('Andorid')
+    react = st.checkbox('React JS')
+    java = st.checkbox('Core Java')
+    javascript = st.checkbox('Javascript')
+    
+    tech_button = st.button('Submit')
+    
+    if tech_button:
+        tech_dict = {
+            'Python': python,
+            'Data Science': datascience,
+            'AI/ML': ai_ml,
+            'Android': andorid,
+            'React JS': react,
+            'Core Java': java,
+            'Javasript': javascript,
+        }
+        st.json(tech_dict)
