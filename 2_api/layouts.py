f9c011ae71fbd4d85ef8fe4956d479cb1634c134
BenceToth <bence.l.toth@gmail.com>
2025-01-16 14:24:37 +0000

    display df column dynamically based on sidebar input

--- 2_api/layouts.py
+++ 2_api/layouts.py
@@ -8,7 +8,20 @@ side_bar = st.sidebar
 # Passing elements to sidebar
 side_bar.header('st.sidebar')
 side_bar.caption('Elements that are added to the sidebar are pinned to the left')
-side_bar.write('This is a sidebar')
 
-# Passing elements to the main page
-st.write('This is the main page')
+# Load tips.csv
+df = pd.read_csv('tips.csv')
+
+df_columns = tuple(df.columns)
+st.write(df_columns)
+
+# Create a Selectbox widget
+selected_column = side_bar.selectbox(
+    "Select the column you want to display",
+    df_columns
+)
+
+side_bar.write('Your selected column is: {}'.format(selected_column))
+
+# Display df
+st.dataframe(df[[selected_column]])
