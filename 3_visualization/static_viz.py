d41a64a18b435389c292641cea2dccce2634ea32
BenceToth <bence.l.toth@gmail.com>
2025-01-17 11:24:06 +0000

    Find the distribution of spending by males and females (various plots)

--- 3_visualization/static_viz.py
+++ 3_visualization/static_viz.py
@@ -12,12 +12,21 @@ df = pd.read_csv('./tips.csv')
 
 st.dataframe(df.head())
 
-## Questions
 st.markdown('---')
 
+# Questions
+## 1. Show the distribution of a categorical feature (pie and bar charts)
+
+# Categorical columns in df
+data_types = df.dtypes
+cat_cols = data_types[data_types == 'object'].index
+
 with st.container():
-    st.header('1. Find the distribution of males and females (pie and bar charts)')
-    value_counts = df['sex'].value_counts()
+    st.header('1. Show the distribution of a categorical feature (pie and bar charts)')
+    
+    # Select feature
+    feature = st.selectbox('Select a feature you want to display on the charts', cat_cols)
+    value_counts = df[feature].value_counts()
     
     col1, col2 = st.columns(2)
     # Pie Chart
@@ -33,9 +42,31 @@ with st.container():
         ax.bar(value_counts.index, value_counts)
         st.pyplot(fig)
         
+    # Display value counts
     with st.expander('Click here to display value counts'):
         st.dataframe(value_counts)
-
+        
+        
+st.markdown('---')
 ## 2. Find the distribution of spending by males and females (boxplot or kdeplot)
+with st.container():
+    st.header('2. Find the distribution of spending by males and females (various plots)')
+    
+    # Select chart
+    chart = ('box', 'violin', 'kdeplot', 'histogram')
+    chart_selection = st.selectbox('Select a chart type', chart)
+    
+    fig, ax = plt.subplots()
+    if chart_selection == 'box':
+        sns.boxplot(x='sex', y='total_bill', data=df, ax=ax)
+    elif chart_selection == 'violin':
+        sns.violinplot(x='sex', y='total_bill', data=df, ax=ax)
+    elif chart_selection == 'kdeplot':
+        sns.kdeplot(x='total_bill', hue='sex', data=df, shade=True, ax=ax)
+    elif chart_selection == 'histogram':
+        sns.histplot(x='total_bill', hue='sex', data=df, ax=ax)
+
+    st.pyplot(fig)
+
 ## 3. Find the distribution of average total_bill across each day by males and females
 ## 4. Find the relationship between total_bill and tip over time (scatter plot)
