c7e5b4c948dea34c5d934fe0a33486a706e97e82
BenceToth <bence.l.toth@gmail.com>
2025-01-20 17:02:43 +0000

    option to change theme

--- profiling-app/data-profiling_app.py
+++ profiling-app/data-profiling_app.py
@@ -9,7 +9,22 @@ st.set_page_config(page_title='Data Profiler', page_icon=':bar_chart:', layout='
 
 # Sidebar
 with st.sidebar:
-    uploaded_file = st.file_uploader('Upload a .csv or .xlsx file (not exceeding 10 MB)')
+    uploaded_file = st.file_uploader('Upload a .csv or .xlsx file (not exceeding 10 MB)', type=['csv', 'xlsx'], accept_multiple_files=False)
+    if uploaded_file is not None:
+        st.write('Modes of Operation')
+        minimal = st.checkbox('Do you want to display a minimal report?')
+        display_mode = st.radio('Select Display Mode', 
+                                options=('Primary', 'Dark', 'Orange'))
+        
+        if display_mode == 'Dark':
+            theme = 'flatly'
+            primary_colors = ['black']
+        elif display_mode == 'Orange':
+            theme = 'united'
+            primary_colors = ['orange']
+        else:
+            theme = None
+            primary_colors = ['blue']
     
 if uploaded_file is not None:
     # Load .csv file
@@ -17,6 +32,8 @@ if uploaded_file is not None:
     
     # Generate report
     with st.spinner('Generating Report...'):
-        pr = ProfileReport(df)
+        pr = ProfileReport(df, minimal=minimal,
+                           html={'style': {'theme': theme,
+                                           'primary_colors': primary_colors}})
         
     st_profile_report(pr)
