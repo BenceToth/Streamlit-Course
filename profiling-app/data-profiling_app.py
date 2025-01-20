a3cd007fb6bfa8714345a9c65da3254ad7ffa66d
BenceToth <bence.l.toth@gmail.com>
2025-01-20 14:38:07 +0000

    replace df display with Profile Report

--- profiling-app/data-profiling_app.py
+++ profiling-app/data-profiling_app.py
@@ -5,6 +5,8 @@ from streamlit_pandas_profiling import st_profile_report
 import sys
 import os
 
+st.set_page_config(page_title='Data Profiler', page_icon=':bar_chart:', layout='wide', initial_sidebar_state='expanded')
+
 # Sidebar
 with st.sidebar:
     uploaded_file = st.file_uploader('Upload a .csv or .xlsx file (not exceeding 10 MB)')
@@ -13,4 +15,8 @@ if uploaded_file is not None:
     # Load .csv file
     df = pd.read_csv(uploaded_file)
     
-    st.dataframe(df.head())
+    # Generate report
+    with st.spinner('Generating Report...'):
+        pr = ProfileReport(df)
+        
+    st_profile_report(pr)
