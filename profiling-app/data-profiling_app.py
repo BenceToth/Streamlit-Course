4e0a12755ae6a9f38f1289fb33f05fe8a1e6d9d9
BenceToth <bence.l.toth@gmail.com>
2025-01-20 13:58:07 +0000

    add data loader

--- profiling-app/data-profiling_app.py
+++ profiling-app/data-profiling_app.py
@@ -1,1 +1,16 @@
-import streamlit as st
+import streamlit as st 
+import pandas as pd 
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+import sys
+import os
+
+# Sidebar
+with st.sidebar:
+    uploaded_file = st.file_uploader('Upload a .csv or .xlsx file (not exceeding 10 MB)')
+    
+if uploaded_file is not None:
+    # Load .csv file
+    df = pd.read_csv(uploaded_file)
+    
+    st.dataframe(df.head())
